7e4499a479ed64a831d19df8ffbd2b8bec7ad2b5
Ale Cozac <ale_ioana98@yahoo.com>
2019-10-02 17:23:23 +0300

    added logs

--- src/main/java/org/fasttrackit/Game.java
+++ src/main/java/org/fasttrackit/Game.java
@@ -10,75 +10,102 @@ public class Game {
 
     private Vehicle competitor1;
     private Vehicle competitor2;
-    private Track[] tracks = new Track[3]; // nr circuite, trebuie specificata lungimea; LUCRU CU SIRURI
-    private List<Vehicle> competitors = new ArrayList<Vehicle>(); //sau doar <>
-
+    private Track[] tracks = new Track[3];
+    private List<Vehicle> competitors = new ArrayList<>();
 
     public void start() throws Exception {
         initializeTracks();
         displayTracks();
+
+        Track selectedTrack = getSelectedTrackFromUser();
+
         initializeCompetitors();
 
         boolean winnerNotKnown = true;
         int competitorsWithoutFuel = 0;
 
-        while(winnerNotKnown && competitorsWithoutFuel < competitors.size()) {
+        while (winnerNotKnown && competitorsWithoutFuel < competitors.size()) {
+            System.out.println("New round...\n");
 
+            // for-each or enhanced for loop
             for (Vehicle vehicle : competitors) {
+                System.out.println("It's " + vehicle.getName() + "'s turn.");
                 double speed = getAccelerationSpeedFromUser();
-                vehicle.accelerate(speed,1);
+                vehicle.accelerate(speed, 1);
 
-                if(selectedTrack.getLength() <= vehicle.getTraveledDistance()) {
+                if (selectedTrack.getLength() <= vehicle.getTraveledDistance()) {
                     winnerNotKnown = false;
                     System.out.println("The winner is: " + vehicle.getName());
                     break;
                 }
 
                 if (vehicle.getFuelLevel() <= 0) {
-                    competitorsWithoutFuel ++;
+                    competitorsWithoutFuel++;
                 }
             }
+        }
 
+    }
+
+    private double getAccelerationSpeedFromUser() {
+        System.out.println("Please enter acceleration speed:");
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+            return scanner.nextDouble();
+        } catch (InputMismatchException e) {
+            System.out.println("You have entered an invalid number.");
+            return getAccelerationSpeedFromUser();
+        }
+    }
+
+    private Track getSelectedTrackFromUser() {
+        System.out.println("Please select a track.");
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+            int userChoice = scanner.nextInt();
+            return tracks[userChoice - 1];
+        } catch (InputMismatchException | ArrayIndexOutOfBoundsException e) {
+            System.out.println("You have entered an invalid number.");
+            // recursion - a method calling itself
+            return getSelectedTrackFromUser();
         }
     }
 
     private void initializeCompetitors() throws Exception {
         int competitorCount = getCompetitorCountFromUser();
 
-        System.out.println("Today's competitors are: ");
+        System.out.println("Today's competitors are:");
 
         for (int i = 0; i < competitorCount; i++) {
             Vehicle competitor = new Vehicle();
             competitor.setName("Competitor " + i);
             competitor.setMaxSpeed(300);
-            competitor.setMileage(ThreadLocalRandom.current().nextDouble(6, 12));
+            competitor.setMileage(
+                    ThreadLocalRandom.current().nextDouble(6, 12));
             competitor.setFuelLevel(80);
 
             System.out.println(competitor);
 
             competitors.add(competitor);
-
-            //competitors.size(); - metoda la lista
         }
 
     }
 
-    private int getCompetitorCountFromUser() throws Exception { //throws != throw
+    private int getCompetitorCountFromUser() throws Exception {
         System.out.println("Please enter number of players.");
         Scanner scanner = new Scanner(System.in);
         try {
-            return scanner.nextInt();             //citire si returnare jucatori
-        } catch (InputMismatchException e) {     //e variabila folosita pt exceptii
-
-            throw new Exception ("You have entered an invalid number.");
-
-        } finally {           //finally optional
+            return scanner.nextInt();
+        } catch (InputMismatchException e) {
+            throw new Exception("You have entered an invalid number.");
+        } finally {
             System.out.println("Finally block is always executed.");
         }
     }
 
-    private void initializeTracks() { //chemare metoda
-
+    private void initializeTracks() {
         Track track1 = new Track();
         track1.setName("Monte Carlo");
         track1.setLength(300);
@@ -90,17 +117,19 @@ public class Game {
         track2.setLength(400);
 
         tracks[1] = track2;
-
     }
 
     private void displayTracks() {
-        System.out.println("Available tracks: ");
+        System.out.println("Available tracks:");
+
         for (int i = 0; i < tracks.length; i++) {
             if (tracks[i] != null) {
-                System.out.println(tracks[i].getName() + " - Length: " + tracks[i].getLength() + " km. ");
+                System.out.println(tracks[i].getName() +
+                        " - Length: " + tracks[i].getLength() + " km.");
             }
         }
 
-
     }
+
+
 }
